9e4cdfe3b9e33dd6921d9eb66bd43aab3f2aa7ac
scchn <scxchn@gmail.com>
2022-11-05 17:15:44 +0800

    部分程式移至 XcapView

--- Tests/XcapAppKitTests/CoreGraphicsTests.swift
+++ Tests/XcapAppKitTests/CoreGraphicsTests.swift
@@ -15,25 +15,4 @@ final class CoreGraphicsTests: XCTestCase {
         XCTAssertEqual(CGFloat(5.0).clean, "5")
     }
     
-    // MAKR: - CGPoint
-    
-    func testCGPoint_mid() {
-        let p1 = CGPoint(x: 0, y: 0)
-        let p2 = CGPoint(x: 10, y: 0)
-        XCTAssertEqual(p1.mid(with: p2), CGPoint(x: 5, y: 0))
-    }
-    
-    func testCGPoint_distance() {
-        let p1 = CGPoint(x: 0, y: 0)
-        let p2 = CGPoint(x: 10, y: 0)
-        XCTAssertEqual(p1.distance(with: p2), 10)
-    }
-    
-    func testCGPoint_extended() {
-        let p = CGPoint(x: 0, y: 0)
-        let extended = p.extended(length: 10, angle: .pi / 2)
-        XCTAssertLessThan(extended.x, 1e-5)
-        XCTAssertEqual(extended.y, 10)
-    }
-    
 }
